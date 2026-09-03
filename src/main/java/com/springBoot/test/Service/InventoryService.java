edbc45f7e4fc2f5bfa14f5931389d433d58c46a5
Navaneethveer3 <navaneeth42.com@gmail.com>
2026-09-03 23:58:17 +0530

    Added theplacing order functionality from the product page removingthe cart only option to place order.

---
+++ src/main/java/com/springBoot/test/Service/InventoryService.java
@@ -0,0 +1,47 @@
+package com.springBoot.test.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.springBoot.test.DTO.InventoryDTO;
+import com.springBoot.test.Model.OrderItem;
+import com.springBoot.test.Model.Product;
+import com.springBoot.test.Repository.ProductRepository;
+
+@Service
+public class InventoryService {
+
+	@Autowired
+	private ProductRepository prodRepo;
+	
+	@KafkaListener(topics = {"order-success", "place-order"}, groupId = "payment-service")
+	public void updateInventory(InventoryDTO dto) throws Exception{
+		int id = dto.getId();
+		int quantity = dto.getQuantity();
+		Product prod = prodRepo.findById(id).orElse(null);
+		if(prod==null) {
+			throw new Exception("Product not found");
+		}
+		if(prod.getQuantity()<quantity) {
+			throw new Exception("Product doesn't has enough stock");
+		}
+		prod.setQuantity(prod.getQuantity()-quantity);
+		prodRepo.save(prod);
+	}
+	
+	@KafkaListener(topics = "order-cancel", groupId = "order-service")
+	public void restoreInventory(InventoryDTO dto) throws Exception{
+		int id = dto.getId();
+		int quantity = dto.getQuantity();
+		Product prod = prodRepo.findById(id).orElse(null);
+		if(prod==null) {
+			throw new Exception("Product not found");
+		}
+		prod.setQuantity(prod.getQuantity()+quantity);
+		prodRepo.save(prod);
+	}
+	
+}
